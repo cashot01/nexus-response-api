9db632c5d6d8e31331720d29343e3ffcfce5cb67
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2025-05-27 08:20:26 -0300

    feat: first commit

---
+++ src/main/java/br/com/fiap/nexus_response_api/NexusResponseApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.nexus_response_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusResponseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusResponseApiApplication.class, args);
+	}
+
+}
