eaf2ab1df645f64c5ccf8cae7664967edbfed5d0
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2025-06-03 13:17:24 -0300

    update EnvironmentalNodeController

--- src/main/java/br/com/fiap/nexus_response_api/controller/EnvironmentalNodeController.java
+++ src/main/java/br/com/fiap/nexus_response_api/controller/EnvironmentalNodeController.java
@@ -1,17 +1,25 @@
 package br.com.fiap.nexus_response_api.controller;
 
 import br.com.fiap.nexus_response_api.model.EnvironmentalNode;
+import br.com.fiap.nexus_response_api.model.Usuario;
+import br.com.fiap.nexus_response_api.repository.EnvironmentalNodeRepository;
 import br.com.fiap.nexus_response_api.service.EnvironmentalNodeService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
-@RestController
+/*@RestController
 @RequestMapping("/environmental")
 @Slf4j
 public class EnvironmentalNodeController {
@@ -52,5 +60,69 @@ public class EnvironmentalNodeController {
     public ResponseEntity<Void> excluirEnvironmentalNode(@PathVariable Long id) {
         environmentalNodeService.excluirEnvironmentalNode(id);
         return ResponseEntity.noContent().build();
+    }
+} */
+@RestController
+@RequestMapping("/environmental-nodes")
+@Slf4j
+public class EnvironmentalNodeController {
+
+    @Autowired
+    private EnvironmentalNodeRepository repository;
+
+    @GetMapping
+    @Cacheable("environmentalNodes")
+    @Operation(description = "Listar todos os nós ambientais", tags = "environmental-nodes", summary = "Lista de nós ambientais")
+    public List<EnvironmentalNode> index(@AuthenticationPrincipal Usuario usuario) {
+        log.info("Buscando todos os nós ambientais");
+        return repository.findByUsuario(usuario);
+    }
+
+    @PostMapping
+    @CacheEvict(value = "environmentalNodes", allEntries = true)
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(responses = {
+            @ApiResponse(responseCode = "400", description = "Falha na validação")
+    })
+    public EnvironmentalNode create(@RequestBody @Valid EnvironmentalNode node, @AuthenticationPrincipal Usuario usuario) {
+        log.info("Cadastrando nó ambiental com temperatura média: " + node.getTempMedia());
+        node.setUsuario(usuario);
+        return repository.save(node);
+    }
+
+    @GetMapping("{id}")
+    public EnvironmentalNode get(@PathVariable Long id, @AuthenticationPrincipal Usuario usuario) {
+        log.info("Buscando nó ambiental com ID: " + id);
+        checkPermission(id, usuario);
+        return getNode(id);
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void destroy(@PathVariable Long id, @AuthenticationPrincipal Usuario usuario) {
+        log.info("Apagando nó ambiental com ID: " + id);
+        checkPermission(id, usuario);
+        repository.delete(getNode(id));
+    }
+
+    @PutMapping("{id}")
+    public EnvironmentalNode update(@PathVariable Long id, @RequestBody @Valid EnvironmentalNode node, @AuthenticationPrincipal Usuario usuario) {
+        log.info("Atualizando nó ambiental com ID: " + id + " - Nova temperatura média: " + node.getTempMedia());
+        checkPermission(id, usuario);
+        node.setIdEnvironmentalNode(id);
+        node.setUsuario(usuario);
+        return repository.save(node);
+    }
+
+    private void checkPermission(Long id, Usuario usuario) {
+        var nodeOld = getNode(id);
+        if (!nodeOld.getUsuario().equals(usuario)) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Você não tem permissão para acessar este recurso.");
+        }
+    }
+
+    private EnvironmentalNode getNode(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Nó ambiental não encontrado"));
     }
 }
